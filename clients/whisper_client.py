3219699792c7c8297b81ee281427c70f5417e71c
Tanya Verma <tverma@cloudflare.com>
2023-08-30 14:50:05 -0700

    client

--- clients/whisper_client.py
+++ clients/whisper_client.py
@@ -2,14 +2,15 @@
 import tritonclient.http as httpclient
 import numpy as np
 import torchaudio
+import argparse
 
 
-if __name__ == "__main__":
+def main(model_name):
     # Setting up client
-    client = httpclient.InferenceServerClient(url="localhost:9000")
+    client = httpclient.InferenceServerClient(url="localhost:8000")
 
     # Load and preprocess audio data
-    audio, sample_rate = torchaudio.load("data/whisper/gettysburg.wav")
+    audio, sample_rate = torchaudio.load("../data/whisper/gettysburg.wav")
     if sample_rate != 16000:
         resampler = torchaudio.transforms.Resample(
             orig_freq=sample_rate, new_freq=16000
@@ -22,9 +23,18 @@ if __name__ == "__main__":
     input_tensor.set_data_from_numpy(data.numpy())
 
     # Query the server
-    response = client.infer(model_name="whisper_base", inputs=[input_tensor])
+    response = client.infer(model_name=model_name, inputs=[input_tensor])
 
     # Get the output tensor
     output_tensor = response.as_numpy("transcription")
 
     print("Transcription:", output_tensor[0].decode("utf-8"))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_name", default="Select between ensemble_model (TODO) and whisper_base"
+    )
+    args = parser.parse_args()
+    main(args.model_name)
